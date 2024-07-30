b92fb501b11042b929a49e0fab689f4337902aac
Himanshu Rai <himanshuraimau9@gmail.com>
2024-07-30 11:21:11 +0530

    create new-user

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
-import Image from "next/image";
+import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const {userId} = await auth();
+  let href = "/new-user"
+  if(userId){
+    href = "/journal"
+  } 
   return (
          <div className="w-screen h-screen bg-black flex  justify-center items-center text-white">
            <div className="w-full max-w-[600px] mx-auto">
             <h1 className="text-3xl mb-4">The best journal app</h1>
             <p className="text-2xl text-white/60 mb-4 ">Get the app  and journal your mood honestly</p>
             <div>
-               <Link href="/journal">
+               <Link href={href}>
                <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">get started</button>
                </Link>
             </div>
