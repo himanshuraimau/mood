591a31fb00a38791aab31e091a633027e2f9763b
Himanshu Rai <himanshuraimau9@gmail.com>
2024-08-15 18:34:34 +0530

    add changes

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -33,20 +33,26 @@ export const PATCH = async (request: Request, { params }: { params: any }) => {
       console.error("Failed to parse analysis result:", error);
       throw new Error("Invalid JSON format in analysis result.");
     }
-
+    
+    if (!analysisData) {
+      return NextResponse.json(
+        { error: "Analysis data is missing or invalid." },
+        { status: 400 }
+      );
+    }
+    
     // Upsert the analysis data into the database
     const updatedAnalysis = await prisma.analysis.upsert({
       where: {
         entryId: updatedEntry.id,
       },
       update: {
-
         mood: analysisData.mood || "",
         summary: analysisData.summary || "",
         subject: analysisData.subject || "",
         negative: analysisData.negative || false,
         color: analysisData.color || "#000000",
-        sentimentScore: analysisData.sentimentScore ,
+        sentimentScore: analysisData.sentimentScore || 0,
       },
       create: {
         userId: user.id,
@@ -56,12 +62,13 @@ export const PATCH = async (request: Request, { params }: { params: any }) => {
         subject: analysisData.subject || "",
         negative: analysisData.negative || false,
         color: analysisData.color || "#000000",
-        sentimentScore: analysisData.sentimentScore,
+        sentimentScore: analysisData.sentimentScore || 0,
       },
       include: {
         entry: true, // Include related journal entry data if needed
       },
     });
+    
 
     // Return the updated journal entry and analysis as JSON
     return NextResponse.json({
